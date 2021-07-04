d873e7c0420ba57692e6f9fa3dd6c75efa00141b
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-04 18:59:59 +0300

    Add more spacing between profile errors

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -2,9 +2,9 @@
     {{-- TODO: Refactor in component <x-error></x-error> --}}
     @if ($errors->any())
         <div class="pt-1 pb-4">
-            <div class="text-white text-md rounded-md font-light bg-red-700 py-4">
+            <div class="text-white text-md rounded-md font-light bg-red-700 py-4 space-y-1">
                 @foreach ($errors->all() as $message)
-                    <h4>{{ $message }}</h4>
+                    <h4 class="px-4">{{ $message }}</h4>
                 @endforeach
             </div>
         </div>
