ccfb59039f6f096ca3eb911f529862fab80bd0e5
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-05 18:36:26 +0300

    Refactor profile to use auth() helper
    Use auth()->user() helper instead of passing user directly from controller to view.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,7 +13,7 @@ class ProfileController extends Controller
 
     public function index()
     {
-        return view('layouts.profile', ['user' => Auth::user()]);
+        return view('layouts.profile');
     }
 
     public function update(Request $request)
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -15,19 +15,19 @@
             </div>
         @endif
     </div>
-    <form method="POST" action="{{ route('profile.update', $user) }}">
+    <form method="POST" action="{{ route('profile.update') }}">
         @method("PUT")
         @csrf
         <div class="max-w-md mx-auto space-y-4">
             <div class="flex items-center space-x-4">
                 <label class="text-white text-md flex-1" for="name">{{ __('Username') }}:</label>
                 <x-input id="name" class="block mt-1 w-3/4 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                    type="text" name="name" value="{{ old('name', $user->name) }}" required />
+                    type="text" name="name" value="{{ old('name', auth()->user()->name) }}" required />
             </div>
             <div class="flex items-center space-x-4">
                 <label class="text-white text-md flex-1" for="name">{{ __('Email') }}:</label>
                 <x-input id="email" class="block mt-1 w-3/4 focus:outline-none focus:ring-2 focus:ring-gray-900"
-                    type="email" name="email" value="{{ old('email', $user->email) }}" required />
+                    type="email" name="email" value="{{ old('email', auth()->user()->email) }}" required />
             </div>
         </div>
 
