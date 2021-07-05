50764544fe2311d0c3da5efb37a0fd3740e60259
Kristers <45801982+chrisd1999@users.noreply.github.com>
2021-07-05 16:47:12 +0300

    Fix localization switch not working
    Refactor to use one middleware, and to properly change localization on switch.

--- app/Http/Middleware/SetDefaultLocaleForUrls.php
+++ app/Http/Middleware/SetDefaultLocaleForUrls.php
@@ -17,7 +17,14 @@ class SetDefaultLocaleForUrls
      */
     public function handle(Request $request, Closure $next)
     {
-        URL::defaults(['locale' => app()->getLocale()]);
+        // TODO: Refactor to change the uri based on request query for aesthetics.
+        // Currently, it changes the locale but has query in the url e.g. ?pl=
+
+        $queryLocale = $request->hasAny(['en', 'pl', 'de']) ? array_keys($request->query())[0] : null;
+        $locale = $queryLocale ?? $request->segment(1);
+
+        app()->setLocale($locale);
+        URL::defaults(['locale' => $locale]);
 
         return $next($request);
     }
--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class SetLocale
 {
@@ -16,7 +17,14 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next)
     {
-        app()->setLocale($request->segment(1));
+        // TODO: Refactor to change the uri based on request query for aesthetics.
+        // Currently, it changes the locale but has query in the url e.g. ?pl=
+
+        $queryLocale = $request->hasAny(['en', 'pl', 'de']) ? array_keys($request->query())[0] : null;
+        $locale = $queryLocale ?? $request->segment(1);
+
+        app()->setLocale($locale);
+        URL::defaults(['locale' => $locale]);
 
         return $next($request);
     }
